f814cfc51f8037be2255b1cbd534d6c3645f07e4
ksope <105855785+ksope@users.noreply.github.com>
2023-04-22 14:04:58 +0100

    Update passport.js
    Added additional callback code (} if (!user.validatePassword(password)) { ...) to LocalStrategy in order to validate any password a user enters

--- passport.js
+++ passport.js
@@ -21,6 +21,11 @@ passport.use(new LocalStrategy({
             message: "Incorrect username ir password.",
         });
     }
+    if (!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect password.'});
+    }
+
     console.log("finished");
     return callback(null, user);
   })
