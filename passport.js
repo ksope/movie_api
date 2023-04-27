d9148fe65bfe9b169dddbce7795286c87dab5d0a
ksope <105855785+ksope@users.noreply.github.com>
2023-04-27 13:45:33 +0100

    Fix User login status 400 error
    Modified User instance method "userSchema.methods.validatePassword" to compare password parameter with correct User property which is "this.Password" rther than "this.parameter"

--- passport.js
+++ passport.js
@@ -10,28 +10,33 @@ let Users = Models.User,
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password'
-}, (username, password, callback) => {
+}, 
+(username, password, callback) => {
   console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
+  Users.findOne({ Username: username })
+  .then((user) => {
+    if(!user){
+        console.log("incorrect username");
+        return callback(null, false, {
+            message: "Incorrect username or password.",
+        });
     }
-
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username.'});
-    }
-
     if (!user.validatePassword(password)) {
       console.log('incorrect password');
       return callback(null, false, {message: 'Incorrect password.'});
     }
 
-    console.log('finished');
+    console.log("finished");
     return callback(null, user);
+  })
+  .catch((error) => {
+    console.log(error);
+    return callback(error, false);
   });
-}));
+}
+)
+);
+
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
